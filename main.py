d64db09b1adb3e742eabcc222f356fd6c3316ba0
frugiam <frugiam@oregonstate.edu>
2024-01-10 18:48:22 -0800

    animal.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Author: Michelle Frugia
+# GitHub username: frugiam
+# Date: 01/10/2024
+# Description: Asks the user for their favorite animal and then prints out that is their favorite animal
+fave_animal = input('wolf:')
+print("Your favorite animal is the " + fave_animal + ".")
